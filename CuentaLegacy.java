c0acd0e26d8a2bc7b9d91d6d90ffa141cb134b7b
Ximena Lozano <yu.xi.lozano@unillanos.edu.co>
2026-08-24 16:28:43 +0000

    avance 1

---
+++ CuentaLegacy.java
@@ -0,0 +1,10 @@
+import java.nio.file.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+public class CuentaLegacy {
+    public static void CuentaLegacy(String[] args) {
+        // Imprime un mensaje en la consola
+        System.out.println("¡Hola, Mundo desde Java!");
+    }
+}
